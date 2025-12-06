e644b3f06af394fc670050327244b82959143ebe
techyJcrafts <joseph.adamu.it@gmail.com>
2025-12-06 21:21:28 +0100

    Models: Add User, ApiKey, and ApiKeyUsage models

---
+++ laravel-api-auth/app/Models/ApiKey.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ApiKey extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'key',
+        'last_used_at',
+    ];
+
+    protected $casts = [
+        'last_used_at' => 'datetime',
+    ];
+
+    protected $hidden = [
+        'key',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function usages()
+    {
+        return $this->hasMany(ApiKeyUsage::class);
+    }
+}
