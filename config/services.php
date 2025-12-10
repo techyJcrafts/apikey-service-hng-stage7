054033081c03a6f1b518939fb1b2713d850e00ef
techyJcrafts <joseph.adamu.it@gmail.com>
2025-12-11 00:03:46 +0100

    fix:updates to the config file based on the new packages installed and the HTTPS allowed scheme for the L5 swagger route

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         ],
     ],
 
+    'paystack' => [
+        'public_key' => env('PAYSTACK_PUBLIC_KEY'),
+        'secret_key' => env('PAYSTACK_SECRET_KEY'),
+        'webhook_secret' => env('PAYSTACK_WEBHOOK_SECRET'),
+    ],
+
 ];
